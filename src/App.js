61037c30d8ad3e6e594b933ffbee67b847cc810d
Jaime Rodriguez <jaimegachs@gmail.com>
2025-08-24 21:07:21 +0200

    feat: add introduction component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarMenu from "./Components/NavbarMenu";
+import Introduction from "./Components/Introduction";
 
 function App() {
   return (
     <div className="App">
       <NavbarMenu></NavbarMenu>
-      <Button variant="primary">Button #1</Button>
+      <Introduction></Introduction>
+      
   
     </div>
   );
