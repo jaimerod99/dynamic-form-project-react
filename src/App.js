b98052c60410a0d3a7fbf025eb5299a3d15d73b1
Jaime Rodriguez <jaimegachs@gmail.com>
2025-08-26 22:12:17 +0200

    feat: add dynamic form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarMenu from "./Components/NavbarMenu";
 import Introduction from "./Components/Introduction";
 import Services from './Components/Services'
+import DynamicForm from "./Components/DynamicForm";
 function App() {
   return (
     <div className="App">
       <NavbarMenu></NavbarMenu>
       <Introduction></Introduction>
       <Services></Services>
+      <DynamicForm></DynamicForm>
   
     </div>
   );
